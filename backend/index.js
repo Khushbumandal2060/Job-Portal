879945094c020066e6f777ff9eaeec855b1fda14
khushi <khusbumandal2060@gmail.com>
2024-09-21 01:13:55 +0545

    packages

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import cookieParser from "cookie-Parser";
+import cookieParser from "cookie-parser";
+import cors from _"cors";
 
 const app = express();
 
@@ -7,7 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const 
+const corsOptions = {
+    origin:'http/localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOption));
 
 const PORT = 3000;
 
